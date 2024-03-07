c6658ecf35a456bd53f6c125de51ed70920e1f17
QuocHung <hungthanh1412hz@gmail.com>
2024-03-07 22:32:24 +0700

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,15 +8,21 @@ import { User } from './typeorm/entites/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'Toilatranhoangnhu1997',
-    database: 'laptrinhdelamgi',
-    entities: [User],
-    synchronize: true
+  imports: [ConfigModule.forRoot(),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => (
+      {
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+      }
+    ),
+    inject: [ConfigService],
   }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
